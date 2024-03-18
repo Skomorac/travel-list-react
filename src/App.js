3eca1c2c7373c302170b8f7bbcff6b5a5a9d77c4
Skomorac <admir.skomorac@outlook.com>
2024-03-18 14:56:31 +0100

    stage 1 basic look

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <h1>React</h1>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1>🌴 Far Away 🧳</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>👜 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
